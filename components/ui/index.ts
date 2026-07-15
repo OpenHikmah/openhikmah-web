a3c69b749a6de5a61c2433383cfeab3694b150fa
Mubashir78 <mubashirrindbaloch@gmail.com>
2026-07-15 10:07:00 +0000

    chore: remove dead components (StreakBadge, Chip)

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -1,6 +1,5 @@
 export { Button, buttonVariants, type ButtonProps } from "./Button";
 export { IconButton, iconButtonVariants, type IconButtonProps } from "./IconButton";
-export { Chip, type ChipProps } from "./Chip";
 export { Card, cardVariants, type CardProps } from "./Card";
 export { Tooltip, TooltipProvider } from "./Tooltip";
 export { Input } from "./Input";
